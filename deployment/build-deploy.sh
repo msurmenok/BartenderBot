4200d9b9ee43b2405092eaeec0532da1ee6f0aac
Pavel Surmenok <surmenok@gmail.com>
2016-11-20 08:18:38 +0000

    Fix folder names in the deployment script

--- deployment/build-deploy.sh
+++ deployment/build-deploy.sh
@@ -1,8 +1,8 @@
 rm -rf /opt/BartenderBuildWorkspace/*
 git pull
-cp /opt/BartenderBot/bot-service-akka/* /opt/BartenderBuildWorkspace
+cp -R /opt/BartenderBot/bot-service-akka/* /opt/BartenderBuildWorkspace
 rm /opt/BartenderBuildWorkspace/src/main/resources/application.conf
-cp /opt/BartenderConfig/application.config /opt/BartenderBuildWorkspace/src/main/resources
+cp /opt/BartenderConfig/application.conf /opt/BartenderBuildWorkspace/src/main/resources
 cd /opt/BartenderBuildWorkspace
 sudo systemctl stop bot-service-akka
 sbt pack
