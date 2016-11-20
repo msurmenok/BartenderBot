a4b60ccdbd4b03d335f2308f1ca5f6c078b9ac01
Pavel Surmenok <surmenok@gmail.com>
2016-11-20 06:34:14 +0000

    Use configuration from a separate folder outside of git on deployment

--- deployment/build-deploy.sh
+++ deployment/build-deploy.sh
@@ -1,7 +1,11 @@
-cd /opt/BartenderBot/bot-service-akka
-sudo git pull
+rm -rf /opt/BartenderBuildWorkspace/*
+git pull
+cp ./botservice-akka/* /opt/BartenderBuildWorkspace
+rm /opt/BartenderBuildWorkspace/src/main/resources/application.conf
+cp /opt/BartenderConfig/application.config /opt/BartenderBuildWorkspace/src/main/resources
+cd /opt/BartenderBuildWorkspace
 sudo systemctl stop bot-service-akka
-sudo sbt pack
-sudo rm -rf /opt/BartenderDeploy/*
-sudo cp target/pack/* /opt/BartenderDeploy -r
+sbt pack
+rm -rf /opt/BartenderDeploy/*
+cp target/pack/* /opt/BartenderDeploy -r
 sudo systemctl start bot-service-akka
